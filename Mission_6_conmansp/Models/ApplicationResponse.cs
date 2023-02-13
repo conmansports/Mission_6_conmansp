381c084184b406cccedfcc52fad3e49ede3f55e1
Connor Sandstrom <conmansp@byu.edu>
2023-02-13 13:17:47 -0700

    Add project files.

---
+++ Mission_6_conmansp/Models/ApplicationResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission_6_conmansp.Models
+{
+    public class ApplicationResponse
+    {
+        [Key]
+        [Required]
+        public int FormID { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string Director { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string Rating { get; set; }
+
+        public bool Edited { get; set; }
+
+        public string LentTo { get; set; }
+
+        [StringLength(25)]
+        public string Notes { get; set; }
+    }
+}
---
+++ Mission_6_conmansp/Models/MovieEntryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_6_conmansp.Models
+{
+    public class MovieEntryContext : DbContext
+    {
+        public MovieEntryContext(DbContextOptions<MovieEntryContext> options) : base(options)
+        {
+            //leave blank for now
+        }
+
+        public DbSet<ApplicationResponse> responses { get; set; }
+    }
+}
